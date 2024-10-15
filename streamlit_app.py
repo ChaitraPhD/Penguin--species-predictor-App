ab74b24ba38f77c5c2070fd4deca8a8e15506f83
Chaitra <chaitraphdrv@gmail.com>
2024-10-15 09:49:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-st.title('🤖 Machine Learning App')
+st.title('🐧 Penguin Species Prediction App')
 
-st.info('This is app builds a machine learning model!')
+st.info('This app predicts Penguins species using a machine learning model!')
 
 with st.expander('Data'):
   st.write('**Raw data**')
